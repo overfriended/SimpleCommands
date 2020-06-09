1f445b4960ae7642786062d78baa975f53c5cbe3
Rocket <mousetvgaming@gmail.com>
2020-06-08 22:25:22 -0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,34 @@
+Config = {}
+
+--[[
+	Permissions
+	
+	Will always be command.commandName.
+	["commandName that you added"] = true
+	or don't add it for something you don't need permissions for.
+]]
+
+Config.Permissions = {
+	["testingwee"] = false
+}
+
+--[[
+	["commandName"] = {"Text here", "chat"} for a simple chat message.
+	OR
+	["commandName"] = {"Text here", "notify"} for a simple notification message that goes above the radar.
+	
+	or for a function:
+	["commandName"] = function(src, args)
+		-- code
+	end
+	
+	To send a notification, you can use the event cc:notify. Remember that this is ENTIRELY CLIENT SIDE. Use 
+	TriggerServerEvent for server events.
+]]
+
+Config.Commands = {
+    ["discord"] = {"The discord can be found here: ~o~discord.gg/MNTBR8u~w~.", "notify"},
+	["testingwee"] = function(src, args)
+		TriggerEvent("cc:notify", "This was a ~g~test~w~!")
+	end
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'bodacious'
+game 'gta5'
+
+name 'Custom Commands'
+author 'Rocket'
+version '1.0.0'
+
+client_script 'config.lua'
+client_script 'client/client.lua'
+
+server_script 'config.lua'
