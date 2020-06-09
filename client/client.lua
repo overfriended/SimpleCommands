72ad9b7be6237c2619a92cc6082c7c592c9f7129
Rocket <mousetvgaming@gmail.com>
2020-06-08 22:26:41 -0300

    Unnecessary Citizen.CreateThread

--- client/client.lua
+++ client/client.lua
@@ -17,22 +17,18 @@ AddEventHandler("cc:notify", function(text, src)
 	notify(text)
 end)	
 
-Citizen.CreateThread(function()
-	for command, message in pairs(Config.Commands) do
-		RegisterCommand(command, function(src, args)
-			if type(message) == "table" then
-				if message[2] == "chat" then
-					send(message[1])
-				else
-					notify(message[1])
-				end
-			elseif type(message) == "function" then
-				message(src, args)
+for command, message in pairs(Config.Commands) do
+	RegisterCommand(command, function(src, args)
+		if type(message) == "table" then
+			if message[2] == "chat" then
+				send(message[1])
 			else
-				notify("~r~Something went wrong.")
+				notify(message[1])
 			end
-		end, Config.Permissions[command] or false)
-		
-		Citizen.Wait(0)
-	end
-end)
+		elseif type(message) == "function" then
+			message(src, args)
+		else
+			notify("~r~Something went wrong.")
+		end
+	end, Config.Permissions[command] or false)
+end
